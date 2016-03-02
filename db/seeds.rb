765a2870cd1e9b6e2d1a1332ca90986758730273
Jennifer W <w.jennifer123@gmail.com>
2016-03-01 19:48:38 -0800

    created seeds file, ready for pushing to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# moods = Mood.create([{ name: 'Chill'}, { name: 'Hyper'}, { name: 'Hangry'}, {name: 'Indecisive' }])
+
+Mood.create(name: 'Chill', image: 'Chill.jpg')
+Mood.create(name: 'Hyper', image: 'hyper_minion.gif')
+Mood.create(name: 'Hangry', image: 'Blueberry Tart.jpeg')
+Mood.create(name: 'Indecisive', image: 'indecisive_cat_shades.jpg')
